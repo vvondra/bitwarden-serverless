b28757a6e6454324a983e368c0341af1c1c7f71b
Vojtech Vondra <vojtavondra@gmail.com>
2017-12-27 12:21:16 +0100

    Cleanup

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ set -e
 serverless deploy --stage test
 API_URL=$(serverless info --stage test --verbose | grep ServiceEndpoint | cut -d":" -f2- | xargs) \
   node_modules/.bin/mocha test
+serverless remove --stage test
