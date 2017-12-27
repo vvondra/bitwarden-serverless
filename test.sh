96a907aa31a8ec97d490441f5beff849621b5c6f
Vojtech Vondra <vojtavondra@gmail.com>
2017-12-27 12:26:40 +0100

    Unique env per test build

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 set -e
 
-serverless deploy --stage test
-API_URL=$(serverless info --stage test --verbose | grep ServiceEndpoint | cut -d":" -f2- | xargs) \
+STAGE=${TRAVIS_COMMIT:-test}
+
+serverless deploy --stage ${STAGE}
+API_URL=$(serverless info --stage ${STAGE} --verbose | grep ServiceEndpoint | cut -d":" -f2- | xargs) \
   node_modules/.bin/mocha test
-serverless remove --stage test
+serverless remove --stage ${STAGE}
