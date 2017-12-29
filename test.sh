67c36bbb50a3e3e4c735f192834e6eecb8fefb3b
Vojtech Vondra <vojtavondra@gmail.com>
2017-12-29 14:42:06 +0100

    Always cleanup tests after failure

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 set -e
 
+function cleanup {
+  serverless remove --stage ${STAGE}
+}
 ./node_modules/.bin/eslint .
 
 STAGE="test${TRAVIS_BUILD_NUMBER}"
@@ -7,4 +10,5 @@ STAGE="test${TRAVIS_BUILD_NUMBER}"
 serverless deploy --stage ${STAGE}
 API_URL=$(serverless info --stage ${STAGE} --verbose | grep ServiceEndpoint | cut -d":" -f2- | xargs) \
   node_modules/.bin/mocha --timeout 5000 test
-serverless remove --stage ${STAGE}
+
+trap finish EXIT
