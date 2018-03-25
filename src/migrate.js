3ef3f9664d47c5182e6ab2b9c9bdad7323d5e32e
Vojtech Vondra <vojtavondra@gmail.com>
2018-03-25 15:13:24 +0200

    migration script

--- src/migrate.js
+++ src/migrate.js
@@ -2,7 +2,8 @@ import speakeasy from 'speakeasy';
 import qrcode from 'qrcode';
 import omit from 'lodash/omit';
 import {
-  User
+  User,
+  Cipher
 } from './lib/models';
 import {
   TYPE_LOGIN,
@@ -14,48 +15,34 @@ import {
 export const migrateHandler = async (event, context, callback) => {
   console.log('Data migration handler triggered', JSON.stringify(event, null, 2));
 
-  if (!event.email) {
-    callback(null, 'E-mail must be supplied');
-    return;
-  }
-
-  let user;
-  try {
-    [user] = (await User.scan()
-      .where('email').equals(event.email.toLowerCase())
-      .execAsync()).Items;
-  } catch (e) {
-    callback(null, 'User not found');
-    return;
-  }
-
   let ciphers;
   try {
-    // TODO await in parallel
-    ciphers = (await Cipher.query(user.get('uuid')).execAsync()).Items;
+    ciphers = (await Cipher.scan().execAsync()).Items;
   } catch (e) {
-    callback(null, 'Server error loading vault items');
+    callback(null, 'Server error loading vault items ' + e.message);
     return;
   }
 
+  let i = 0;
   ciphers.forEach(async (cipher) => {
     const version = cipher.get('version');
-
+    console.log("Checking cipher " + cipher.get('uuid') + ' with version ' + version);
     switch (version) {
       case 1:
         // up-to-date
         console.log("Already up-to-date")
         break;
-      case null:
+      default:
+        i = i + 1;
         const fields = {
           version: 1,
         };
         const data = cipher.get('data');
 
         if (data) {
-          fields.name = data.Name;
-          fields.notes = data.Notes;
-          fields.fields = data.Fields;
+          fields.name = data.Name || null;
+          fields.notes = data.Notes || null;
+          fields.fields = data.Fields || null;
           if (cipher.get('type') == TYPE_LOGIN) {
             fields.login = {
               Uris: {
@@ -64,21 +51,23 @@ export const migrateHandler = async (event, context, callback) => {
               }
             };
           }
-        }
 
-        const fmap = {
-          [TYPE_LOGIN]: "login",
-          [TYPE_NOTE]: "securenote",
-          [TYPE_CARD]: "card",
-          [TYPE_IDENTITY]: "identity",
-        }
+          const fmap = {
+            [TYPE_LOGIN]: "login",
+            [TYPE_NOTE]: "securenote",
+            [TYPE_CARD]: "card",
+            [TYPE_IDENTITY]: "identity",
+          }
 
-        fields[fmap[cipher.get('type')]] = omit(data, ['Name', 'Notes', 'Fields', 'Uri']);
+          fields[fmap[cipher.get('type')]] = omit(data, ['Name', 'Notes', 'Fields', 'Uri']);
+        }
 
         console.log("Updating with fields", fields);
         cipher.set(fields);
         const wait = await cipher.updateAsync();
         break;
     }
-  })
+  });
+
+  callback(null, 'Migrated ' + i + ' ciphers.');
 };
