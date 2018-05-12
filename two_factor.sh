8ee894f2cc0aba3d3608911677337da617aad1d1
Vojtech Vondra <vojtavondra@gmail.com>
2018-05-12 21:59:43 +0200

    Support for region variable in two factor script

--- two_factor.sh
+++ two_factor.sh
@@ -2,15 +2,16 @@
 set -e
 
 STAGE=${STAGE:-prod}
+REGION=${REGION:-us-east-1}
 
 echo "Please provide the e-mail of the user you want to setup 2FA for:"
 
 read email
 
-serverless invoke -s $STAGE -f two_factor_setup -d"{\"email\": \"${email}\"}"
+serverless invoke -s "$STAGE" -r "$REGION" -f two_factor_setup -d"{\"email\": \"${email}\"}"
 
 echo "Copy the URL above into your browser, scan the QR code in your authenticator app, and enter a code once to verify:"
 
 read code
 
-serverless invoke -s $STAGE -f two_factor_complete -d"{\"email\": \"${email}\",\"code\": \"${code}\"}"
+serverless invoke -s "$STAGE" -r "$REGION" -f two_factor_complete -d"{\"email\": \"${email}\",\"code\": \"${code}\"}"
