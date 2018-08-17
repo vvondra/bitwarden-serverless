a0e76bb1969873e2821d8f69353295c74343d079
Vojtěch Vondra <vvondra@users.noreply.github.com>
2018-08-17 12:33:28 +0200

    Update prelogin.js

--- src/prelogin.js
+++ src/prelogin.js
@@ -1,8 +1,10 @@
+const KDF_PBKDF2 = 0;
+
 export const handler = async (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      Kdf: 0,
+      Kdf: KDF_PBKDF2,
       KdfIterations: 5000,
     }),
   });
